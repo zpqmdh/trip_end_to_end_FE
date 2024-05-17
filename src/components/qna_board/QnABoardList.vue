b9c1ad9f335353be87d1038d51a299c5f1dc8d33
zpqmdh <ysys1130@gmail.com>
2024-05-17 15:15:58 +0900

    feat: QnA 게시판 리스트 화면 구성

--- src/components/qna_board/QnABoardList.vue
+++ src/components/qna_board/QnABoardList.vue
@@ -1,5 +1,55 @@
-<script setup></script>
+<script setup>
+import QnABoardArticleItem from "@/components/qna_board/item/QnABoardArticleItem.vue";
+import { ref, onMounted } from "vue";
+const articles = ref([
+  {
+    articleNo: "1",
+    subject: "hihi",
+    hit: "122",
+    userId: "김싸피",
+    registerTime: "2024-05-02",
+  },
+  {
+    articleNo: "2",
+    subject: "hihi",
+    hit: "122",
+    userId: "김싸피",
+    registerTime: "2024-05-02",
+  },
+  {
+    articleNo: "3",
+    subject: "hihi",
+    hit: "122",
+    userId: "김싸피",
+    registerTime: "2024-05-02",
+  },
+]);
+
+const getArticleList = () => {};
+
+onMounted(() => {
+  getArticleList();
+});
+</script>
 <template>
-  <div>QnA Board List</div>
+  <h1>❔ 문의 게시판</h1>
+  <table class="table table-hover">
+    <thead>
+      <tr class="text-center">
+        <th scope="col">글번호</th>
+        <th scope="col">제목</th>
+        <th scope="col">조회수</th>
+        <th scope="col">작성자</th>
+        <th scope="col">작성일</th>
+      </tr>
+    </thead>
+    <tbody>
+      <QnABoardArticleItem
+        v-for="article in articles"
+        :key="article.articleNo"
+        :article="article"
+      ></QnABoardArticleItem>
+    </tbody>
+  </table>
 </template>
 <style scoped></style>
--- src/components/qna_board/item/QnABoardArticleItem.vue
+++ src/components/qna_board/item/QnABoardArticleItem.vue
@@ -1,5 +1,20 @@
-<script setup></script>
+<script setup>
+defineProps({ article: Object });
+</script>
 <template>
-  <div>QnA Board Article Item</div>
+  <tr class="text-center">
+    <td>{{ article.articleNo }}</td>
+    <td>
+      <router-link
+        :to="{
+          name: 'qna-detail',
+          params: { id: article.articleNo },
+        }"
+        >{{ article.subject }}</router-link
+      >
+    </td>
+    <td>{{ article.userId }}</td>
+    <td>{{ article.hit }}</td>
+    <td>{{ article.registerTime }}</td>
+  </tr>
 </template>
-<style scoped></style>
