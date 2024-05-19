09c1bcb2cc32f2318b3aa7e2a9a4b987e2bceea0
zpqmdh <ysys1130@gmail.com>
2024-05-20 03:38:54 +0900

    feat: notice_board 리스트, 상세, 작성 페이지 구현

--- src/components/notice_board/NoticeBoardDetail.vue
+++ src/components/notice_board/NoticeBoardDetail.vue
@@ -1,5 +1,90 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+import { localAxios } from "@/util/http-commons.js";
+import { useRoute } from "vue-router";
+
+const local = localAxios();
+const route = useRoute();
+
+const notice = ref({});
+onMounted(() => {
+  getDetail(route.params.id);
+});
+const getDetail = (id) => {
+  local.get("/notice/" + id).then(({ data }) => {
+    console.log(data);
+    notice.value = data;
+  });
+};
+</script>
+
 <template>
-  <div>Notice Board Detail</div>
+  <div class="container">
+    <h2>{{ notice.subject }}</h2>
+    <hr />
+    <div class="meta">
+      <span class="author">👤 작성자: {{ notice.nickname }}</span>
+      <span class="divider">|</span>
+      <span class="views">👁 조회수: {{ notice.hit }}</span>
+      <span class="divider">|</span>
+      <span class="time">🕒 {{ notice.registerTime }}</span>
+    </div>
+    <div class="content">{{ notice.content }}</div>
+    <RouterLink :to="{ name: 'notice-list' }" class="back-link"
+      >목록으로</RouterLink
+    >
+  </div>
 </template>
-<style scoped></style>
+
+<style scoped>
+.container {
+  margin-top: 20px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+h2 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+hr {
+  border: none;
+  border-top: 1px solid #ccc;
+  margin-bottom: 20px;
+}
+
+.meta {
+  margin-bottom: 20px;
+}
+
+.author,
+.views,
+.time {
+  margin-right: 10px;
+  font-size: 14px;
+  color: #666;
+}
+
+.divider {
+  color: #999;
+}
+
+.content {
+  font-size: 16px;
+  line-height: 1.6;
+  margin-bottom: 20px;
+}
+
+.back-link {
+  display: inline-block;
+  margin-top: 20px;
+  color: blue;
+  text-decoration: none;
+}
+
+.back-link:hover {
+  text-decoration: underline;
+}
+</style>
--- src/components/notice_board/NoticeBoardList.vue
+++ src/components/notice_board/NoticeBoardList.vue
@@ -1,5 +1,69 @@
-<script setup></script>
+<script setup>
+import NoticeBoardArticleItem from "./item/NoticeBoardArticleItem.vue";
+import NoticePageNavigation from "./item/NoticePageNavigation.vue";
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+import { localAxios } from "@/util/http-commons.js";
+
+const route = useRoute();
+const local = localAxios();
+
+const articles = ref([]);
+
+const currentPage = ref(1);
+const totalPage = ref(0);
+const key = ref("");
+const word = ref("");
+
+const getArticleList = () => {
+  local
+    .get(
+      `/notice/list?pgno=${currentPage.value}&key=${key.value}&word=${word.value}`
+    )
+    .then(({ data }) => {
+      articles.value = data.articles;
+      currentPage.value = data.pageNavigation.currentPage;
+      totalPage.value = data.pageNavigation.totalPageCount;
+
+      console.log(data);
+    });
+};
+
+onMounted(() => {
+  getArticleList();
+});
+
+const onPageChange = (val) => {
+  currentPage.value = val;
+  getArticleList();
+};
+</script>
 <template>
-  <div>Notice Board List</div>
+  <div class="container">
+    <h1>❔ 공지사항</h1>
+    <table class="table table-hover">
+      <thead>
+        <tr class="text-center">
+          <th scope="col">글번호</th>
+          <th scope="col">제목</th>
+          <th scope="col">조회수</th>
+          <th scope="col">작성자</th>
+          <th scope="col">작성일</th>
+        </tr>
+      </thead>
+      <tbody>
+        <NoticeBoardArticleItem
+          v-for="article in articles"
+          :key="article.articleNo"
+          :article="article"
+        ></NoticeBoardArticleItem>
+      </tbody>
+    </table>
+    <NoticePageNavigation
+      :current-page="currentPage"
+      :total-page="totalPage"
+      @pageChange="onPageChange"
+    ></NoticePageNavigation>
+  </div>
 </template>
 <style scoped></style>
