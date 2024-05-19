07a00821e11aa7a29aae974d0cdbde22b8aa0fb6
jihyeon-yu <fbwlgus6@naver.com>
2024-05-20 01:45:17 +0900

    feat: 챗봇 오버레이 구현

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { RouterView } from "vue-router";
 import Navigator from "@/components/common/Navigator.vue";
+import Chatbot from "@/components/common/ChatBot.vue";
 </script>
 
 <template>
   <div>
     <Navigator />
     <router-view />
+    <Chatbot />
   </div>
 </template>
 
